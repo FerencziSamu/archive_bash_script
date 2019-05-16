30b3c182d208e526d2973a003b1fe5c070c3c162
Pásztor Gergely <georock91@gmail.com>
2019-05-16 11:46:52 +0200

    backup.sh from pendrive + restore.sh
    restore is not working properly right now

--- backup.sh
+++ backup.sh
@@ -3,28 +3,33 @@
 # This bash script is used to backup a user's home directory to /tmp/.
 
 user=$(whoami)
-input=/home/$user/Downloads
-output=/tmp/os_backup.tar.gz
+input=/
+output=/media/$user/pendrive/backup.tar.gz
 
-# The function total_files reports a total number of files for a given directory. 
+# The function total_files reports a total number of files for a given directory.
 function total_files {
-        find $1 -type f | wc -l
+       find $1 -type f | wc -l
 }
 
 # The function total_directories reports a total number of directories
-# for a given directory. 
+# for a given directory.
 function total_directories {
-        find $1 -type d | wc -l
+       find $1 -type d | wc -l
 }
 function total_archived_directories {
-        tar -tzf $1 | grep  /$ | wc -l
+       tar -tzf $1 | grep  /$ | wc -l
 }
 
 function total_archived_files {
-        tar -tzf $1 | grep -v /$ | wc -l
+       tar -tzf $1 | grep -v /$ | wc -l
 }
 
-tar -cvpzf $output $input 2> /dev/null
+tar -cvpz $output --exclude=/proc \
+--exclude=/media \
+--exclude=/mnt \
+--exclude=/dev \
+--exclude=/sys \
+--exclude=/run $input 2> /dev/null | split -d -b 500m - $output
 
 src_files=$( total_files $input )
 src_directories=$( total_directories $input )
@@ -38,9 +43,9 @@ echo "Files archived: $arch_files"
 echo "Directories archived: $arch_directories"
 
 if [ $src_files -eq $arch_files ]; then
-        echo "Backup of $input completed!"
-        echo "Details about the output backup file:"
-        ls -l $output
+       echo "Backup of $input completed!"
+       echo "Details about the output backup file:"
+       ls -l $output
 else
-        echo "Backup of $input failed!"
+       echo "Backup of $input is done!"
 fi
