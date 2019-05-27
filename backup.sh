be595e49bff4057a6a91b525311d32d059ebf059
denagyantal <denagyantal@gmailc.om>
2019-05-27 11:04:48 +0200

    print result and comments

--- backup.sh
+++ backup.sh
@@ -24,23 +24,25 @@ function total_archived_files {
        tar -tzf $1 | grep -v /$ | wc -l
 }
 
-tar -cvpzf $output --exclude=/backup --one-file-system $input > /backup/tar_stdout.log 2> /backup/tar_stderr.log
+tar -cvpzf $output --exclude=/backup --exclude="swapfile" --exclude=/proc/* --one-file-system $input > /backup/tar_stdout.log 2> /backup/tar_stderr.log
 
+
+# To check the result and save it into a txt 
 src_files=$( total_files $input )
 src_directories=$( total_directories $input )
 
 arch_files=$( total_archived_files $output )
 arch_directories=$( total_archived_directories $output )
 
-echo "Files to be included: $src_files"
-echo "Directories to be included: $src_directories"
-echo "Files archived: $arch_files"
-echo "Directories archived: $arch_directories"
+echo "Files to be included: $src_files" >> result.txt
+echo "Directories to be included: $src_directories" >> result.txt 
+echo "Files archived: $arch_files" >> result.txt
+echo "Directories archived: $arch_directories" >> result.txt
 
 if [ $src_files -eq $arch_files ]; then
-       echo "Backup of $input completed!"
-       echo "Details about the output backup file:"
+       echo "Backup of $input completed!" >> result.txt
+       echo "Details about the output backup file:" >> result.txt
        ls -l $output
 else
-       echo "Backup of $input is failed!"
+       echo "Backup of $input is failed!" >> result.txt
 fi
