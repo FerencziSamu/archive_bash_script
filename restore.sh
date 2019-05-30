a55e4aaa1377dfcb923a6486056a7f556bedd424
Pásztor Gergely <georock91@gmail.com>
2019-05-30 17:37:01 +0200

    + log

--- restore.sh
+++ restore.sh
@@ -2,7 +2,8 @@
 
 user=ubuntupaszti
 
-sudo tar -xvpzf /home/$user/backup.tar.gz -C /
+sudo tar -xvpzf /home/$user/backup.tar.gz -C / \
+3>&1 1>/backup/restore_stdout.log 2>&3- | tee -a /backup/restore_stderr.log
 
 echo "Restore is complete!"
 
--- restoressh.sh
+++ restoressh.sh
@@ -4,7 +4,8 @@ user1=ubuntupaszti
 user2=ubuntupaszti
 ip_adress=192.168.50.124
 
-scp $user1@$ip_adress:/home/$user1/backup.tar.gz /home/$user2 && sudo tar -xvpzf /home/$user2/backup.tar.gz -C /
+scp $user1@$ip_adress:/home/$user1/backup.tar.gz /home/$user2 && sudo tar -xvpzf /home/$user2/backup.tar.gz -C / \
+3>&1 1>/backup/restore_stdout.log 2>&3- | tee -a /backup/restore_stderr.log
 
 echo "Restore is complete!"
 
