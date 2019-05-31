8965c43025ebdeef0fd4dadf368e8f73de924d17
Ferenczi Sámuel <33907257+FerencziSamu@users.noreply.github.com>
2019-05-31 10:49:26 +0200

    Delete backupssh.sh

--- backupssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-user=$(whoami)
-input=/
-output=/home/ubuntupaszti/backup/backup.tar.gz
-
-ssh ubuntupaszti@192.168.50.167 tar cvpzf - --exclude=/backup --one-file-system / > /home/$user/backup/backup.tar.gz
-
-echo "Backup of $input is complete!"
