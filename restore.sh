b34ef29601fba2c87f5ec595377484e91c0d5c49
Ferenczi Sámuel <33907257+FerencziSamu@users.noreply.github.com>
2019-05-31 10:46:17 +0200

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 user=$(whoami)
 
-sudo tar -xvpzf /home/ubuntupaszti/backup/backup.tar.gz -C /
-
-
-<<<<<<< HEAD
-sudo tar -xvpzf /backup/backup_base.tar.gz -C /
-=======
->>>>>>> 6193ba03f91877921cd7178c93aa3c56698227bf
+sudo tar -xvpzf /home/$user/backup/backup.tar.gz -C /
